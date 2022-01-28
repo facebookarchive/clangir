51ff8082b16d4158db5faed570a6bcfe187d67c4
Nathan Lanza <nathan@lanza.io>
2022-01-27 19:29:59 -0500

    [CIR][NFC] Move CIRGenFunction code to its own file

--- clang/lib/CIR/CIRGenModule.cpp
+++ clang/lib/CIR/CIRGenModule.cpp
@@ -70,64 +70,6 @@ using llvm::makeArrayRef;
 using llvm::ScopedHashTableScope;
 using llvm::SmallVector;
 using llvm::StringRef;
-using llvm::Twine;
-
-CIRGenFunction::CIRGenFunction() = default;
-TypeEvaluationKind CIRGenFunction::getEvaluationKind(QualType type) {
-  type = type.getCanonicalType();
-  while (true) {
-    switch (type->getTypeClass()) {
-#define TYPE(name, parent)
-#define ABSTRACT_TYPE(name, parent)
-#define NON_CANONICAL_TYPE(name, parent) case Type::name:
-#define DEPENDENT_TYPE(name, parent) case Type::name:
-#define NON_CANONICAL_UNLESS_DEPENDENT_TYPE(name, parent) case Type::name:
-#include "clang/AST/TypeNodes.inc"
-      llvm_unreachable("non-canonical or dependent type in IR-generation");
-
-    case Type::Auto:
-    case Type::DeducedTemplateSpecialization:
-      llvm_unreachable("undeduced type in IR-generation");
-
-    // Various scalar types.
-    case Type::Builtin:
-    case Type::Pointer:
-    case Type::BlockPointer:
-    case Type::LValueReference:
-    case Type::RValueReference:
-    case Type::MemberPointer:
-    case Type::Vector:
-    case Type::ExtVector:
-    case Type::ConstantMatrix:
-    case Type::FunctionProto:
-    case Type::FunctionNoProto:
-    case Type::Enum:
-    case Type::ObjCObjectPointer:
-    case Type::Pipe:
-    case Type::BitInt:
-      return TEK_Scalar;
-
-    // Complexes.
-    case Type::Complex:
-      return TEK_Complex;
-
-    // Arrays, records, and Objective-C objects.
-    case Type::ConstantArray:
-    case Type::IncompleteArray:
-    case Type::VariableArray:
-    case Type::Record:
-    case Type::ObjCObject:
-    case Type::ObjCInterface:
-      return TEK_Aggregate;
-
-    // We operate on atomic values according to their underlying type.
-    case Type::Atomic:
-      type = cast<AtomicType>(type)->getValueType();
-      continue;
-    }
-    llvm_unreachable("unknown type kind!");
-  }
-}
 
 namespace cir {
 
